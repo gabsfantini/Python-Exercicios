67f6e45cb617fa5ab08e3aa23ce0e73df65c4a0f
Gabriel Fantini <gabrielfantini91@gmail.com>
2024-03-27 10:20:39 -0300

    Ex 002
    Mundo 001 - Aula 001
    #Exercício Python 002: Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.

---
+++ Mundo 001 - Aula 01/Ex 002.py
@@ -0,0 +1,4 @@
+#Exercício Python 002: Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+
+name = (input('Escreva seu nome: '))
+print('Olá {} Bem-Vindo!'.format(name))

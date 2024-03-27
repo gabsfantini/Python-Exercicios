0071a961f0413945659eddc43ef8bf2a01af3df8
Gabriel Fantini <gabrielfantini91@gmail.com>
2024-03-27 10:27:02 -0300

    Exercício 003
    Mundo 001 - Aula 001
    #Exercício Python 003: Crie um programa que leia dois números e mostre a soma entre eles.

---
+++ Mundo 001 - Aula 01/Ex 003.py
@@ -0,0 +1,6 @@
+#Exercício Python 003: Crie um programa que leia dois números e mostre a soma entre eles.
+
+num_1 = int(input('Digite um valor: '))
+num_2 = int(input('Digite um valor: '))
+soma = num_1 + num_2
+print('A soma de {} + {} é igual a {} '.format(num_1, num_2, soma))

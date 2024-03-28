1da15b872d7bcad19cf0f1f6f33bbec00ff9691c
Gabriel Fantini <gabrielfantini91@gmail.com>
2024-03-28 09:06:07 -0300

    Ex 006
    Mundo 001 - Aula 001
    #Exercício Python 006: Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.

--- Mundo 001 - Aula 01/Ex 006.py
+++ Mundo 001 - Aula 01/Ex 006.py
@@ -5,4 +5,4 @@ n = int(input('Digite um número: '))
 n1 = n * 2
 n2 = n * 3
 n3 = sqrt(n)
-print('Analisando o número {}, o seu dobro é {}, o seu triplo é {} e a sua raiz quadrada é {:.0f}'.format(n,n1,n2,n3)) 
+print('Analisando o número {}, o seu dobro é {}, o seu triplo é {} e a sua raiz quadrada é {:.0f}'.format(n,n1,n2,n3))

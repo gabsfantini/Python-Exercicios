6142f55b492f11cabaa4c03eb1d9fefae9deb52f
Gabriel Fantini <gabrielfantini91@gmail.com>
2024-03-28 15:03:14 -0300

    Ex 001
    Mundo 001
    #Exercício 1 - printar na tela a frase "Olá, Mundo!" usando a linguagem de Python.

---
+++ Mundo 001/Ex 001.py
@@ -0,0 +1,10 @@
+msg = 'Olá, Mundo!'
+print(msg)
+if msg == 'Olá, Mundo!':
+    print('De qual planeta você é?')
+    mundo = input('Digite seu Planeta: ')
+if mundo == 'Terra':
+    print('Meus pesames... :(')
+else:
+    print('Ainda bem que você não é terráqueo...')
+   

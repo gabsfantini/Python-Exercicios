0966e456fdfb506c02a66cd9a205bec780334c21
Gabriel Fantini <gabrielfantini91@gmail.com>
2024-04-01 09:15:00 -0300

    Ex 011
    Mundo 001
    #Exercício Python 011: Faça um programa que leia a largura e a altura de uma parede em metros, calcule a sua área e a quantidade de tinta necessária para pintá-la, sabendo que cada litro de tinta pinta uma área de 2 metros quadrados.

---
+++ Mundo 001/Ex 011.py
@@ -0,0 +1,7 @@
+#Exercício Python 011: Faça um programa que leia a largura e a altura de uma parede em metros, calcule a sua área e a quantidade de tinta necessária para pintá-la, sabendo que cada litro de tinta pinta uma área de 2 metros quadrados.
+
+comprimento = float(input('Digite a comprimento da sua parede: '))
+altura = float(input('Digite a altura da sua parede: '))
+area = altura * comprimento
+tinta = area * 2
+print('A sua parede tem {}m² e você vai precisar de {} litros de tinta para pintar ela toda.'.format(area, tinta))
